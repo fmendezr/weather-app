30a4d9308f4fb3a868fcac57cb0f04ae13c41701
Felix Mendez <mendez.felixrn@gmail.com>
2022-10-30 15:33:08 -0400

    update git ignore to remove dist file and create async function that can get weather data using the long and lat of location

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import "./style.css";
+
+async function getWeather(lat, long) {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=6079b902d16c1d19a4a0e658ed40c5a2`,
+    { mode: "cors" }
+  );
+  const weatherData = await response.json();
+  console.log(weatherData);
+}
