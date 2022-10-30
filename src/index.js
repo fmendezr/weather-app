a63db9c8bce12690f9976f65f72fe78bfe23af6d
Felix Mendez <mendez.felixrn@gmail.com>
2022-10-30 16:17:50 -0400

    create function that takes name of lcoaiton and gives back coordinates

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import "./style.css";
 
-async function getWeather(lat, long) {
+async function getWeather(lat, lon) {
   const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=6079b902d16c1d19a4a0e658ed40c5a2`,
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=6079b902d16c1d19a4a0e658ed40c5a2`,
     { mode: "cors" }
   );
   const weatherData = await response.json();
   console.log(weatherData);
+}
+
+async function getCoordinates(name) {
+  const response = await fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${name}&limit=1&appid=6079b902d16c1d19a4a0e658ed40c5a2`
+  );
+  const data = await response.json();
+  const latLon = {
+    lat: await data[0].lat,
+    lon: await data[0].lon,
+  };
+  return latLon;
 }
