891fc85ae2cb6f022624c76b3f0c232017888d47
Felix Mendez <mendez.felixrn@gmail.com>
2022-10-30 23:30:53 -0400

    Create fuction that takes name and returns weather data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./style.css";
 
-async function getWeather(lat, lon) {
+async function getCurrentWeather(lat, lon) {
   const response = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=6079b902d16c1d19a4a0e658ed40c5a2`,
     { mode: "cors" }
@@ -20,3 +20,10 @@ async function getCoordinates(name) {
   };
   return latLon;
 }
+
+async function weather(name) {
+  const coordinates = getCoordinates(name);
+  getCurrentWeather((await coordinates).lat, (await coordinates).lon);
+}
+
+weather("london");
